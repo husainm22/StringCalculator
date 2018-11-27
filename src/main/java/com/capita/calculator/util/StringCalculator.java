2103081ea7a48239a48e71d6e58091b39b331aed
husainm22 <45360262+husainm22@users.noreply.github.com>
2018-11-27 17:30:34 +0530

    Custom evaluation
    Custom evaluation

--- src/main/java/com/capita/calculator/util/StringCalculator.java
+++ src/main/java/com/capita/calculator/util/StringCalculator.java
@@ -1,5 +1,6 @@
 package com.capita.calculator.util;
 
+import java.util.Stack;
 import java.util.regex.Pattern;
 
 import javax.script.ScriptEngine;
@@ -8,22 +9,79 @@ import javax.script.ScriptException;
 
 public class StringCalculator {
 
-	public static String evaluate(String expression) {
-		if(expression==null || expression.length()==0) {
-			return "EMPTY EXPRESSION";
-		}
-		ScriptEngineManager mgr = new ScriptEngineManager();
-	    ScriptEngine engine = mgr.getEngineByName("JavaScript");
-	    try {
-			return String.valueOf((Integer) engine.eval(expression));
-		} catch (ScriptException e) {
-			return "INVALID EXPRESSION";
-		}
-	}
-	
 	public static boolean validate(String expression) {
 		return Pattern.matches("[a-zA-Z0-9\\\\+\\\\-\\\\*/\\\\(\\\\)]*", expression);
 	}
-	
 
+	/**
+	 * Evaluate the string
+	 * 				
+	 * @param expression
+	 * @return
+	 */
+	public static String evaluate(String expression) 
+	{ 
+		if(expression==null || expression.length()==0) {
+			return "EMPTY EXPRESSION";
+		}
+		char[] chars = expression.toCharArray(); 
+
+		Stack<Integer> numbers = new Stack<Integer>(); 
+
+		Stack<Character> operands = new Stack<Character>(); 
+
+		for (int i = 0; i < chars.length; i++) 
+		{ 
+			if (chars[i] >= '0' && chars[i] <= '9') 
+			{ 
+				numbers.push(Integer.parseInt(chars[i]+"")); 
+			} else if (chars[i] == '(') {
+				operands.push(chars[i]); 
+			} else if (chars[i] == ')') { 
+				while (operands.peek() != '(') {
+					numbers.push(eval(operands.pop(), numbers.pop(), numbers.pop())); 
+				}
+				operands.pop(); 
+			} else if (chars[i] == '+' || chars[i] == '-' || 
+					chars[i] == '*' || chars[i] == '/') { 
+				while (!operands.empty() && isPrecedence(chars[i], operands.peek())) {
+					numbers.push(eval(operands.pop(), numbers.pop(), numbers.pop()));
+				}
+				operands.push(chars[i]); 
+			} 
+		} 
+
+		while (!operands.empty()) {
+			numbers.push(eval(operands.pop(), numbers.pop(), numbers.pop()));
+		}
+		
+		return numbers.pop()+""; 
+	} 
+
+	private static int eval(char operand, int val1, int val2) 
+	{ 
+		switch (operand) 
+		{ 
+			case '+': 
+				return val2 + val1; 
+			case '-': 
+				return val2 - val1; 
+			case '*': 
+				return val2 * val1; 
+			case '/': 
+				return val2 / val1; 
+			default:
+				return 0;
+		} 
+	} 
+
+	private static boolean isPrecedence(char op1, char op2) 
+    { 
+        if (op2 == '(' || op2 == ')') 
+            return false; 
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-')) 
+            return false; 
+        else
+            return true; 
+    } 
 }
